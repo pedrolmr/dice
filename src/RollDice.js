2b97cfb96c7a9c85e994ef32e37359b4f6b9347b
pedro <pedroluismontesinosrivera@gmail.com>
2019-06-04 00:05:25 -0400

    animated text in button

--- src/RollDice.js
+++ src/RollDice.js
@@ -9,13 +9,19 @@ class RollDice extends Component{
     }
     constructor(props){
         super(props);
-        this.state = {die1: "one", die2: "one"}
+        this.state = {die1: "one", die2: "one", rolling:false}
     }
     roll = () => {
+        //creating random rolls
         const newDie1 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)]
         const newDie2 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)]
 
-        this.setState({die1: newDie1, die2: newDie2})
+        this.setState({die1: newDie1, die2: newDie2, rolling:true})
+
+        //wait one second then set rolling to false
+        setTimeout(() => {
+            this.setState({rolling: false})
+        },1000);
 
     }
     render(){
@@ -25,7 +31,9 @@ class RollDice extends Component{
                     <Die face={this.state.die1}/>
                     <Die face={this.state.die2} />
                 </div>
-                <button onClick={this.roll}>Roll Dice</button>
+                <button onClick={this.roll} disabled={this.state.rolling}>
+                    {this.state.rolling ? 'rolling...' : 'roll dice'}
+                </button>
             </div>
         );
     }
