99262fd55729b47f6229969b0718eabe5b24e721
pedro <pedroluismontesinosrivera@gmail.com>
2019-06-03 23:49:24 -0400

    added the rolldice component to make use of state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
-import Die from './Die';
+import RollDice from './RollDice';
 
 function App() {
   return (
     <div className="App">
-      <Die face="five" />
-      <Die face="six" />
-      <Die face="three" />
-      <Die face="two" />
+      <RollDice />
     </div>
   );
 }
