5d1493295a947f66832c58f7f59cb3ba30066394
pedro <pedroluismontesinosrivera@gmail.com>
2019-06-03 23:33:27 -0400

    wrote the Die component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Die from './Die';
 
 function App() {
   return (
     <div className="App">
-      <h1>pedro</h1>
+      <Die face="five" />
+      <Die face="six" />
+      <Die face="three" />
+      <Die face="two" />
     </div>
   );
 }
