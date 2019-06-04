4af16cd7a5598591708eb0a56cdcba08a0eb8901
pedro <pedroluismontesinosrivera@gmail.com>
2019-06-03 23:55:13 -0400

    styled button

--- src/RollDice.js
+++ src/RollDice.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Die from './Die';
 
+import './RollDice.css'
+
 class RollDice extends Component{
     static defaultProps = {
         sides: ["one", "two", "three", "four", "five", "six"]
@@ -18,9 +20,11 @@ class RollDice extends Component{
     }
     render(){
         return (
-            <div>
-                <Die face={this.state.die1}/>
-                <Die face={this.state.die2} />
+            <div className="RollDice">
+                <div className="RollDice-container">
+                    <Die face={this.state.die1}/>
+                    <Die face={this.state.die2} />
+                </div>
                 <button onClick={this.roll}>Roll Dice</button>
             </div>
         );
